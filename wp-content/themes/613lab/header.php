3e19110b8a99bb2845cffc7b8c813241212c5f25
Brydon Holsey <brydon.holsey@electronictenant.com>
2020-10-25 22:12:59 -0400

    add images to header

--- wp-content/themes/613lab/header.php
+++ wp-content/themes/613lab/header.php
@@ -15,7 +15,7 @@
         <div class="row">
           <div class="col-lg-12">
             <div class="home-header-image"><?php the_post_thumbnail(''); ?></div>
-            <div class="overlay"> <img src="images/home-overlay.png" alt=""> </div>
+            <div class="overlay"> <img src="http://localhost:8888/613LAB/wp-content/uploads/2020/10/home-overlay.png" alt=""> </div>
 
           </div>
         </div>
