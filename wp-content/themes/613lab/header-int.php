e26066f6054081830c0be9155411f6cfae2d84bd
Brydon Holsey <brydon.holsey@electronictenant.com>
2020-11-10 00:01:49 -0500

    fix menu

--- wp-content/themes/613lab/header-int.php
+++ wp-content/themes/613lab/header-int.php
@@ -17,46 +17,62 @@
   </head>
 
   <body <?php body_class(); ?>>
-    <header class="header">
+    <header id="header-int" class="header">
       <div class="container-fluid header-content">
         <div class="row header-top">
               <div class="col-lg-12 header-icons">
                 <div class="social-icons">
-                <ul>
+                  <ul>
                     <li><a><i class="fab fa-facebook-f"></i></a></li>
                     <li><a><i class="fab fa-linkedin-in"></i></a></li>
                     <li><a><i class="fab fa-instagram"></i></a></li>
                   </ul>
                 </div>
-
-                <!------------ mobile menu ------------>
-                <div>
-                  <div id="hamburger">
-                    <i class="fas fa-bars"></i>
-                  </div>
-                  <div class="mobile-menu">
-                    <div class="mobile-menu__items">
-                      <?php
-                        if(has_nav_menu('main-menu')){
-                          wp_nav_menu(array(
-                            'theme_location'  => 'main-menu',
-                            'container_class' => 'main_menu'
-                          ));
-                        }else{
-                          echo "<p>Please select a main menu through the dashboard</p>";
-                        }
-                      ?>
-                    </div>
-                  </div>
-                </div>
-              
-<!------------- mobile menu end --------------->
-
               </div>
- 
+
+        </div>
+        <div class="page-title">
+          <h1><?php echo get_the_title(); ?></h1>
         </div>
         
 
 
       </div>
     </header>
+
+    <!------------ mobile menu ------------>
+    <nav>
+                  <div id="hamburger">
+                    <div class="line" id="one"></div>
+                    <div class="line" id="two"></div>
+                    <div class="line" id="three"></div>
+                  </div>
+                  <div class="mobile-menu">
+                      <div><img src="wp-content/themes/613lab/images/logo-613-hz.png" alt=""></div>
+                      <div id="slide-out-nav" class="mobile-menu__items">
+                        <?php
+                          if(has_nav_menu('main-menu')){
+                            wp_nav_menu(array(
+                              'theme_location'  => 'main-menu',
+                              'container_class' => 'main_menu'
+                            ));
+                          }else{
+                            echo "<p>Please select a main menu through the dashboard</p>";
+                          }
+                        ?>
+                      </div>
+                      <div>
+                        <p>1234 Main Street <br />
+                        Ontario, Canada</p>
+                        <div class="social-icons">
+                  <ul>
+                    <li><a><i class="fab fa-facebook-f"></i></a></li>
+                    <li><a><i class="fab fa-linkedin-in"></i></a></li>
+                    <li><a><i class="fab fa-instagram"></i></a></li>
+                  </ul>
+                </div>
+                      </div>
+                  </div>
+                </nav>
+              
+<!------------- mobile menu end --------------->
--- wp-content/themes/613lab/header.php
+++ wp-content/themes/613lab/header.php
@@ -28,38 +28,9 @@
                     <li><a><i class="fab fa-instagram"></i></a></li>
                   </ul>
                 </div>
-
-                <!------------ mobile menu ------------>
-                <nav>
-                  <div id="hamburger">
-                    <i class="fas fa-bars"></i>
-                  </div>
-                  <div class="mobile-menu">
-                      <div><img src="wp-content/themes/613lab/images/logo-613-hz.png" alt=""></div>
-                      <div id="slide-out-nav" class="mobile-menu__items">
-                        <?php
-                          if(has_nav_menu('main-menu')){
-                            wp_nav_menu(array(
-                              'theme_location'  => 'main-menu',
-                              'container_class' => 'main_menu'
-                            ));
-                          }else{
-                            echo "<p>Please select a main menu through the dashboard</p>";
-                          }
-                        ?>
-                      </div>
-                  </div>
-                </nav>
-              
-<!------------- mobile menu end --------------->
-
               </div>
-             
-
-
-                
-              
         </div>
+
         <div class="row center-header">
           <div class="col-md-5">
             <div class="home-logo">
@@ -82,3 +53,40 @@
 
       </div>
     </header>
+
+       <!------------ mobile menu ------------>
+       <nav>
+                  <div id="hamburger">
+                    <div class="line" id="one"></div>
+                    <div class="line" id="two"></div>
+                    <div class="line" id="three"></div>
+                  </div>
+                  <div class="mobile-menu">
+                      <div><img src="wp-content/themes/613lab/images/logo-613-hz.png" alt=""></div>
+                      <div id="slide-out-nav" class="mobile-menu__items">
+                        <?php
+                          if(has_nav_menu('main-menu')){
+                            wp_nav_menu(array(
+                              'theme_location'  => 'main-menu',
+                              'container_class' => 'main_menu'
+                            ));
+                          }else{
+                            echo "<p>Please select a main menu through the dashboard</p>";
+                          }
+                        ?>
+                      </div>
+                      <div>
+                        <p>1234 Main Street <br />
+                        Ontario, Canada</p>
+                        <div class="social-icons">
+                  <ul>
+                    <li><a><i class="fab fa-facebook-f"></i></a></li>
+                    <li><a><i class="fab fa-linkedin-in"></i></a></li>
+                    <li><a><i class="fab fa-instagram"></i></a></li>
+                  </ul>
+                </div>
+                      </div>
+                  </div>
+                </nav>
+              
+<!------------- mobile menu end --------------->
